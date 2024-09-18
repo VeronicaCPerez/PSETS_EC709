1ac895187814266b93b7e39f8100abeb6103ec7f
Verónica Pérez <71244136+VeronicaCPerez@users.noreply.github.com>
2024-09-17 21:52:21 -0400

    added random code for ketnel

--- PSET1/PSET1_Q2.r
+++ PSET1/PSET1_Q2.r
@@ -378,7 +378,46 @@ data_q23 <- data[, c("log_gas", "log_price", "log_income", "log_distance")]
 
 bandwidths <- c(0.01, 0.05, 0.08, 0.1, 0.15, 0.2, 0.25, 0.3)
 
+## code from  https://bookdown.org/egarpor/NP-UC3M/kre-ii-multmix.html 
 
+## need to adapt to current data
+
+# Sample data from a bivariate regression
+# n <- 300
+# set.seed(123456)
+# X <- mvtnorm::rmvnorm(n = n, mean = c(0, 0),
+#                       sigma = matrix(c(2, 0.5, 0.5, 1.5), nrow = 2, ncol = 2))
+# m <- function(x) 0.5 * (x[, 1]^2 + x[, 2]^2)
+# epsilon <- rnorm(n)
+# Y <- m(x = X) + epsilon
+# 
+# # Plot sample and regression function
+# rgl::plot3d(x = X[, 1], y = X[, 2], z = Y, xlim = c(-3, 3), ylim = c(-3, 3),
+#             zlim = c(-4, 10), xlab = "X1", ylab = "X2", zlab = "Y")
+# lx <- ly <- 50
+# x_grid <- seq(-3, 3, l = lx)
+# y_grid <- seq(-3, 3, l = ly)
+# xy_grid <- as.matrix(expand.grid(x_grid, y_grid))
+# rgl::surface3d(x = x_grid, y = y_grid,
+#                z = matrix(m(xy_grid), nrow = lx, ncol = ly),
+#                col = "lightblue", alpha = 1, lit = FALSE)
+# 
+# # Local constant fit
+# 
+# # An alternative for calling np::npregbw without formula
+# bw0 <- np::npregbw(xdat = X, ydat = Y, regtype = "lc")
+# kre0 <- np::npreg(bws = bw0, exdat = xy_grid) # Evaluation grid is now a matrix
+# rgl::surface3d(x = x_grid, y = y_grid,
+#                z = matrix(kre0$mean, nrow = lx, ncol = ly),
+#                col = "red", alpha = 0.25, lit = FALSE)
+# 
+# # Local linear fit
+# bw1 <- np::npregbw(xdat = X, ydat = Y, regtype = "ll")
+# kre1 <- np::npreg(bws = bw1, exdat = xy_grid)
+# rgl::surface3d(x = x_grid, y = y_grid,
+#                z = matrix(kre1$mean, nrow = lx, ncol = ly),
+#                col = "green", alpha = 0.25, lit = FALSE)
+# rgl::rglwidget()
 
 
 # 2_3_2 locally linear ----------------------------------------------------
